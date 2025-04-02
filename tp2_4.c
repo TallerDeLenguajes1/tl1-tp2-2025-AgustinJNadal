76c55d9b158d45bca9bc4446eac1e6803d88f1bf
AgustinJNadal <nadaljorge32@gmail.com>
2025-04-02 01:32:55 -0300

    declaradas las funciones

---
+++ tp2_4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct 
+{
+    int velocidad; // Velocidad de procesamiento en GHz (valor entre 1 y 3)
+    int anio; // Año de fabricación (valor entre 2015 y 2024)
+    int cantidad_nucleos; // Cantidad de núcleos (valor entre 1 y 8)
+    char *tipo_cpu; // Tipo de procesador (apuntador a cadena de caracteres)
+}typedef compu;
+
+void listarPCs(compu *pcs, int num_pcs);
+void mostrarMasVieja(compu *pcs, int num_pcs);
+void mostrarMasVeloz(compu *pcs, int num_pcs);
