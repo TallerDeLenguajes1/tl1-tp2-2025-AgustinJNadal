5715682202070a61847618309f40e65603b18f50
AgustinJNadal <nadaljorge32@gmail.com>
2025-04-02 00:04:58 -0300

    nose que se me olvido guardar xd

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -10,8 +10,8 @@ int main() {
     srand(time(NULL));
     for (i = 0; i < N; i++)
     {
-        vt[i] = 1 + rand() % 100;
-        printf("%.2f\n", vt[i]);
+        *(vt+i) = 1 + rand() % 100;
+        printf("%.2f\n", *(vt+i));
     }
     
     return 0;
